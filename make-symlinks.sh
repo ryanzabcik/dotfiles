ee64d66b1489a164c2fb7a9a7f14a25c4b0de72a
Ryan Zabcik <ryanzabcik@gmail.com>
2013-04-04 12:50:27 -0500

    initial commit of a script to make symlinks in home

---
+++ make-symlinks.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+dotglob_status=$(shopt | grep dotglob | rev | cut -d " " -f 1 | rev | tr -d "[ \011]")
+
+shopt -s dotglob
+
+for file in dotfiles/*;
+do
+    ln -s dotfiles/$file $file
+
+[ "$dotglob_status" == "off"] && shopt -u dotglob
