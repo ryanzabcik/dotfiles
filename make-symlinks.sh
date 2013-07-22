628d188cc00a18ff62c796c51351c05f145885fd
Ryan Zabcik <ryanzabcik@gmail.com>
2013-07-22 10:14:33 -0500

    added a space

--- make-symlinks.sh
+++ make-symlinks.sh
@@ -1,7 +1,7 @@
 rm .bashrc
-ln -s git/dotfiles.bash
-ln -s git/dotfiles.bashrc
-ln -s git/dotfiles.vim
-ln -s git/dotfiles.vimrc
-ln -s git/dotfiles.vimcrypt
-ln -s git/dotfiles.tmux.conf
+ln -s git/dotfiles/.bash
+ln -s git/dotfiles/.bashrc
+ln -s git/dotfiles/.vim
+ln -s git/dotfiles/.vimrc
+ln -s git/dotfiles/.vimcrypt
+ln -s git/dotfiles/.tmux.conf
