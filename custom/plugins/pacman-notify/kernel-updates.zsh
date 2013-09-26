cb490afab631f3fc00535b5e62c3956ffd2ff6bf
Ryan Zabcik <ryanzabcik@gmail.com>
2013-09-25 23:18:06 -0500

    Created pacman-notify plugin

---
+++ custom/plugins/pacman-notify/kernel-updates.zsh
@@ -0,0 +1,7 @@
+if [ -x "/usr/bin/pacman" ]; then
+    current_version=$(uname -r | cut -d- -f-2)
+    package_version=$(pacman -Q linux | cut -d' ' -f2)
+    if [ ! "$current_version" = "$package_version" ]; then
+        echo -e "${fg_bold[red]}Your kernel needs to be updated ($current_version -> $package_version). Reboot the system at the next earliest convenience."
+    fi
+fi
---
+++ custom/plugins/pacman-notify/pacman-notify.plugin.zsh
@@ -0,0 +1,3 @@
+local dir=$(dirname $0)
+source "$dir/kernel-updates.zsh"
+source "$dir/package-updates.zsh"
