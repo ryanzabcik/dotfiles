e3ad88caa8806def2a7f3075a6697ae1a1a9a880
Ryan Zabcik <ryanzabcik@gmail.com>
2013-12-13 02:45:18 -0600

    now typing 'okay' will quell notifications about updates

--- custom/plugins/pacman-notify/package-updates.zsh
+++ custom/plugins/pacman-notify/package-updates.zsh
@@ -1,16 +1,16 @@
-if [ -x "/usr/bin/pacman" ]; then
+if [ -x "/usr/bin/pacman" ] && [ ! -e /tmp/okay-pacman-notify ]; then
     # one-line string containing all the updates availible
     pacman_updates_availible=$(cut -d " " -f 1 <(pacman -Qu) | tr "\n" " ")
     # enumerate the updates
     pacman_num_updates_availible=$(echo $pacman_updates_availible | wc -w)
 
-    if [[ $pacman_num_updates_availible > 0 ]]
-        then 
-            if [[ $pacman_num_updates_availible == 1 ]]
-                then pacman_updates_string="\e[1;33mThere is an update availible: $pacman_updates_availible"
-                else pacman_updates_string="\e[1;33mThere are $pacman_num_updates_availible updates availible: $pacman_updates_availible"
-            fi
+    if [ $pacman_num_updates_availible -gt 0 ]; then 
+        if [ $pacman_num_updates_availible -eq 1 ]
+            then pacman_updates_string="\e[1;33mThere is an update availible: $pacman_updates_availible"
+            else pacman_updates_string="\e[1;33mThere are $pacman_num_updates_availible updates availible: $pacman_updates_availible"
+        fi
     fi
+
     COLUMNS=$(tput cols)
     pacman_updates_string_wc=$(echo $pacman_updates_string | wc -w)
     # while the length of the update string is longer than columns:
--- custom/plugins/pacman-notify/pacman-notify.plugin.zsh
+++ custom/plugins/pacman-notify/pacman-notify.plugin.zsh
@@ -1,3 +1,9 @@
 local dir=$(dirname $0)
 source "$dir/kernel-updates.zsh"
 source "$dir/package-updates.zsh"
+
+# Use this function to quell notifications until next boot.
+function okay {
+    touch /tmp/okay-pacman-notify
+    clear
+}
