0fbd9ab7371e2934c34b81f4dcf761bec2e8be4a
Ryan Zabcik <ryanzabcik@gmail.com>
2013-12-13 02:51:04 -0600

    if the at program exists, use it to delete okay file after 24 hours

--- custom/plugins/pacman-notify/pacman-notify.plugin.zsh
+++ custom/plugins/pacman-notify/pacman-notify.plugin.zsh
@@ -5,5 +5,6 @@ source "$dir/package-updates.zsh"
 # Use this function to quell notifications until next boot.
 function okay {
     touch /tmp/okay-pacman-notify
+    which at && at now + 24 hours <<<"rm /tmp/okay-pacman-notify"
     clear
 }
