7598d27bdb66d3f2f11f348de5638d903bc3ec56
Ryan Zabcik <ryanzabcik@gmail.com>
2014-02-11 00:26:46 -0600

    makes symlinks for zsh too

--- make-symlinks.sh
+++ make-symlinks.sh
@@ -1,6 +1,8 @@
 rm .bashrc
 ln -s git/dotfiles/.bash
 ln -s git/dotfiles/.bashrc
+ln -s git/dotfiles/.zsh
+ln -s git/dotfiles/.zshrc
 ln -s git/dotfiles/.vim
 ln -s git/dotfiles/.vimrc
 ln -s git/dotfiles/.vimcrypt
