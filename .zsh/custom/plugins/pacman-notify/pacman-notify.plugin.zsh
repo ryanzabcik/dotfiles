220cd8402f078ad14e5654ce6b48ce4ced737b9c
Ryan Zabcik <ryanzabcik@gmail.com>
2014-04-17 20:31:45 -0500

    added okay! which doesn't delete after 24 hours

--- .zsh/custom/plugins/pacman-notify/pacman-notify.plugin.zsh
+++ .zsh/custom/plugins/pacman-notify/pacman-notify.plugin.zsh
@@ -2,9 +2,15 @@ local dir=$(dirname $0)
 source "$dir/kernel-updates.zsh"
 source "$dir/package-updates.zsh"
 
-# Use this function to quell notifications until next boot.
+# Use this function to quell notifications until next boot or 24 hours.
 function okay {
     touch /tmp/okay-pacman-notify
     which at && at now + 24 hours <<<"rm /tmp/okay-pacman-notify"
+    clear
+}
+
+# This one won't be deleted in 24 hours.
+function okay! {
+    touch /tmp/okay-pacman-notify
     clear
 }
