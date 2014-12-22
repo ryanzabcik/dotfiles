d322f250a57c6035d51328baf2d16c496e03729d
Ryan Zabcik <ryanzabcik@gmail.com>
2014-12-21 18:19:50 -0600

    wrapper for netctl

---
+++ .zsh/custom/plugins/netctl-wrapper/netctl-wrapper.plugin.zsh
@@ -0,0 +1,21 @@
+# Make netctl(1) a little friendlier
+function netctl_wrapper() {
+    local -a netctl
+    netctl=(sudo netctl)
+    local current=$($netctl list | grep '*' | cut -c 3-)
+    if [[ -z $1 ]] || [[ $1 == l* ]]; then
+        $netctl list
+    elif [[ $1 == stop ]]; then
+        if [[ -z $2 ]]; then
+            $netctl stop-all
+        else
+            $netctl stop $2
+        fi
+    elif [[ $1 == restart ]] || [[ $1 == status ]]; then
+        $netctl $1 $current
+    fi
+}
+
+# optional shorthand aliases
+alias net=netctl_wrapper # net(8) is already a program if you have samba installed; type \net to use it
+alias n=netctl_wrapper
