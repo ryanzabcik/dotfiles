278488c6fd634393e0567d876c1afe754910565a
Ryan Zabcik <ryanzabcik@gmail.com>
2018-08-02 18:42:45 -0500

    pacman-notify: fix mismatch of versions

--- .zsh/custom/plugins/pacman-notify/kernel-updates.zsh
+++ .zsh/custom/plugins/pacman-notify/kernel-updates.zsh
@@ -1,6 +1,6 @@
 if [ -x "/usr/bin/pacman" ] && [ ! -e /tmp/okay-pacman-notify ]; then
-    current_version=$(uname -r | cut -d- -f-2)
-    package_version=$(pacman -Q linux | cut -d' ' -f2)
+    current_version=$(uname -r | cut -d- -f1)
+    package_version=$(pacman -Q linux | cut -d' ' -f2 | cut -d- -f1 | cut -d. -f-3)
     if [ "$current_version" != "$package_version" ]; then
         echo -e "${fg_bold[red]}Your kernel needs to be updated ($current_version -> $package_version). Reboot the system at the next earliest convenience."
     fi
